780b26e162747c7092aabcd213ee1368af393b9b
Konstantin Kostenski <konstantin_kostenski@yahoo.com>
2019-02-22 13:15:44 +0200

    Some additions.

--- LinearDataStructuresImplementations/ArrayStack.cs
+++ LinearDataStructuresImplementations/ArrayStack.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 
-namespace DoublyLinkedListImplementation
+namespace LinearDataStructuresImplementations
 {
     public class ArrayStack<T> : IEnumerable<T>
     {
--- LinearDataStructuresImplementations/DoublyLinkedList.cs
+++ LinearDataStructuresImplementations/DoublyLinkedList.cs
@@ -2,7 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 
-namespace DoublyLinkedListImplementation
+namespace LinearDataStructuresImplementations
 {
     public class DoublyLinkedList<T> : IEnumerable<T>
     {
