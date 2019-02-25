42b339bf3b4bd31649d0438b14cbb19a06bda20a
Konstantin Kostenski <konstantin_kostenski@yahoo.com>
2019-02-25 13:24:55 +0200

    Linked stack commit.

---
+++ LinearDataStructuresImplementations/LinkedStack.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace LinearDataStructuresImplementations
+{
+    public class LinkedStack<T> : IEnumerable<T>
+    {
+        private Node topNode;
+        private int nodeNumber;
+
+        private class Node
+        {
+            public Node(T value)
+            {
+                this.Value = value;
+            }
+
+            public T Value { get; set; }
+            public Node Previous { get; set; }
+        }
+
+        public void Push (T value)
+        {
+            if (this.topNode == null)
+            {
+                this.topNode = new Node(value);
+                this.nodeNumber++;
+            }
+            else
+            {
+                var temp = this.topNode;
+                this.topNode = new Node(value);
+                this.topNode.Previous = temp;
+                this.nodeNumber++;
+            }
+        }
+
+        public T Pop()
+        {
+            if (this.topNode == null)
+            {
+                throw new ArgumentException();
+            }
+
+            var value = this.topNode.Value;
+            var temp = this.topNode;
+            this.topNode = this.topNode.Previous;
+            this.nodeNumber--;
+            return value;
+        }
+
+
+        public T[] ToArray()
+        {
+            var current = this.topNode;
+            int index = 0;
+            T[] array = Array.Empty<T>();
+
+            while (current == null)
+            {
+                array = new T[this.nodeNumber];
+                array[index++] = current.Value;
+                current = current.Previous;
+            }
+
+            return array;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            var current = this.topNode;
+
+            while (current != null)
+            {
+                yield return current.Value;
+                current = current.Previous;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+    }
+}
--- LinearDataStructuresImplementations/StartUp.cs
+++ LinearDataStructuresImplementations/StartUp.cs
@@ -9,6 +9,23 @@ namespace LinearDataStructuresImplementations
             //Testing area for the collections.
             DoublyLinkedList<int> list = new DoublyLinkedList<int>();
             ArrayStack<int> stack = new ArrayStack<int>();
+            LinkedStack<int> linkedStack = new LinkedStack<int>();
+
+            linkedStack.Push(1);
+            linkedStack.Push(2);
+            linkedStack.Push(3);
+            linkedStack.Push(4);
+            linkedStack.Push(5);
+
+            linkedStack.Pop();
+            linkedStack.Pop();
+            linkedStack.Pop();
+
+
+            foreach (var number in linkedStack)
+            {
+                Console.WriteLine(number);
+            }
 
             stack.Push(1);
             stack.Push(2);
@@ -16,10 +33,10 @@ namespace LinearDataStructuresImplementations
             stack.Push(4);
             stack.Push(5);
 
-            foreach (var number in stack)
-            {
-                Console.WriteLine(number);
-            }
+            //foreach (var number in stack)
+            //{
+            //    Console.WriteLine(number);
+            //}
 
             list.AddLast(5);
             list.AddLast(4);
@@ -30,10 +47,10 @@ namespace LinearDataStructuresImplementations
             list.RemoveLast();
             list.RemoveLast();
 
-            foreach (var number in list)
-            {
-                Console.WriteLine(number);
-            }
+            //foreach (var number in list)
+            //{
+            //    Console.WriteLine(number);
+            //}
         }
     }
 }
